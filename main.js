35dc4a8375a7293a401ff90f12a12d533a02e834
wayn <1669738430@qq.com>
2022-09-27 00:27:25 +0800

    init

--- main.js
+++ main.js
@@ -1,32 +1,155 @@
-const { app, BrowserWindow } = require('electron');
+const { app, ipcMain, BrowserWindow, Menu, Notification, Tray, nativeImage, shell, dialog } = require('electron');
 const path = require('path');
 
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 390,
-    height: 844,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      webSecurity: false,
-      devTools: true
-    },
-  });
+// this should be placed at top of main.js to handle setup events quickly
+if (handleSquirrelEvent()) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return;
+}
 
-  win.loadURL("http://oa.wayn.xin");
+function handleSquirrelEvent() {
+  if (process.argv.length === 1) {
+    return false;
+  }
+
+  const ChildProcess = require('child_process');
+  const path = require('path');
+
+  const appFolder = path.resolve(process.execPath, '..');
+  const rootAtomFolder = path.resolve(appFolder, '..');
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+  const exeName = path.basename(process.execPath);
+
+  const spawn = function (command, args) {
+    let spawnedProcess, error;
+
+    try {
+      spawnedProcess = ChildProcess.spawn(command, args, { detached: true });
+    } catch (error) { }
+
+    return spawnedProcess;
+  };
+
+  const spawnUpdate = function (args) {
+    return spawn(updateDotExe, args);
+  };
+
+  const squirrelEvent = process.argv[1];
+  switch (squirrelEvent) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+      // Optionally do things such as:
+      // - Add your .exe to the PATH
+      // - Write to the registry for things like file associations and
+      //   explorer context menus
+
+      // Install desktop and start menu shortcuts
+      spawnUpdate(['--createShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-uninstall':
+      // Undo anything you did in the --squirrel-install and
+      // --squirrel-updated handlers
+
+      // Remove desktop and start menu shortcuts
+      spawnUpdate(['--removeShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-obsolete':
+      // This is called on the outgoing version of your app before
+      // we update to the new version - it's the opposite of
+      // --squirrel-updated
+
+      app.quit();
+      return true;
+  }
 };
 
-app.whenReady().then(() => {
-  createWindow();
+let mainWindow;
+if (process.defaultApp) {
+  console.log(process.defaultApp)
+  console.log(process.execPath)
+  console.log(process.argv)
+  if (process.argv.length >= 2) {
+    app.setAsDefaultProtocolClient('wayn', process.execPath, [path.resolve(process.argv[1])])
+  }
+} else {
+  app.setAsDefaultProtocolClient('wayn')
+}
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
+const gotTheLock = app.requestSingleInstanceLock()
+
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+  })
+
+
+  /*隐藏electron创听的菜单栏*/
+  Menu.setApplicationMenu(null)
+  const createWindow = () => {
+    mainWindow = new BrowserWindow({
+      width: 390,
+      height: 844,
+      resizable: false,
+      webPreferences: {
+        nodeIntegrationInWorker: true,
+        preload: path.join(__dirname, 'preload.js'),
+        devTools: true
+      },
+    });
+
+    mainWindow.webContents.openDevTools();
+    mainWindow.loadURL("http://127.0.0.1:5173/");
+  };
+
+
+  app.whenReady().then(() => {
+    createWindow();
+    const icon = nativeImage.createFromPath('img/fish.ico')
+    const contextMenu = Menu.buildFromTemplate([
+      { label: 'Item1', type: 'radio' },
+      { label: 'Item2', type: 'radio' },
+      { label: 'Item3', type: 'radio', checked: true },
+      { label: 'Item4', type: 'radio' }
+    ])
+
+    let tray = new Tray(icon)
+    tray.setContextMenu(contextMenu)
+    tray.setToolTip('This is my application')
+    tray.setTitle('This is my title')
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+      }
+    });
+  });
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
     }
   });
-});
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+
+  app.on('open-url', (event, url) => {
+    dialog.showErrorBox('Welcome Back', `You arrived from: ${url}`)
+  })
+}
+
+// Handle window controls via IPC
+ipcMain.on('shell:open', () => {
+  const pageDirectory = __dirname.replace('app.asar', 'app.asar.unpacked')
+  const pagePath = path.join('wayn://', pageDirectory, 'index.html')
+  shell.openExternal(pagePath)
+})
--- preload.js
+++ preload.js
@@ -1,8 +1,9 @@
-const { contextBridge } = require('electron')
+const { contextBridge, ipcRenderer } = require('electron')
 
-contextBridge.exposeInMainWorld('versions', {
+contextBridge.exposeInMainWorld('shell', {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
   electron: () => process.versions.electron,
+  open: () => ipcRenderer.send('shell:open'),
   // 能暴露的不仅仅是函数，我们还可以暴露变量
 })
